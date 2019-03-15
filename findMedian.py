db5bc49d7fbf19acdb17590e427daaab667ae63e
Jeffrey Lynn <jeffreyr.lynn@gmail.com>
2019-03-14 22:19:25 -0400

    Create findMedian.py

---
+++ findMedian.py
@@ -0,0 +1,12 @@
+#find median of a list
+
+def median(lst):
+  sorted_list = sorted(lst)
+  if len(sorted_list) % 2 != 0:
+  	index = len(newlist) // 2
+	return sorted_list[index]
+  elif len(sorted_list) % 2 == 0
+	index_1 = len(sorted_list) / 2 - 1
+	index_2 = len(sorted_list) / 2
+	mean = (sorted_list[index_1] + sorted_list[index_2]) / 2.0
+	return mean
